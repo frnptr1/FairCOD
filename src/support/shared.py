e25cfd6127a8063e24ec4f146096a18093dce707
Your Name <you@example.com>
2024-11-26 11:16:20 +0100

    new file shared acting as bridge for information and objects sharing between different scritps

---
+++ src/support/shared.py
@@ -0,0 +1,21 @@
+"""
+Module for shared variables across the application.
+"""
+# Session-related variables
+time_session = None
+# full path of project position
+project_root = None
+# full path of support folder
+support_modules_path = None
+# full path of weighting config
+weighting_config_path = None
+# Paths and filenames
+db_config_path = None
+# aggregation selected
+aggregation = None
+# db filepath
+db_path = None
+# columns to_drop
+columns_to_drop = None
+# db connection
+DBConnection = None
